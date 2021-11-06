21c29c04293c4eb0febe93de120c21acdc6d07a6
Pedro Paulo <pedro@adderesolutions.com>
2021-11-06 00:57:11 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import './App.css';
+import { ButtonHover } from './components/buttonHover';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header-landing'>
+        <div id='header-image'>
+          <img src="https://res.cloudinary.com/dtqaoxaot/image/upload/v1636160404/Hack%20Faesa%202021/Group_1_tyelge.svg" />
+        </div>
+      </header>
+      <main>
+        <section className='section-landing'>
+          <div className='section-landing-title'>Unidos por um mundo melhor</div>
+          <div className='section-landing-description'>Compartilhe ideias e soluções em prol de uma sociedade com responsabilidade ecológica. De forma facilitada e acessível</div>
+          <div className='section-landing-button'><button id='section-landing-button-meetnow'>Conheça agora</button></div>
+        </section>
+
+        <div className='section-landing-image'><img id='section-landing-img' src='https://res.cloudinary.com/dtqaoxaot/image/upload/v1636160404/Hack%20Faesa%202021/garota_cf0j6e.svg' /></div>
+
+        <section className='section-landing-simple'>
+          <div className='section-landing-simple-text'>Nossa solução é utilizada em mais de 1,000 empresas</div>
+          <div className='section-landing-text-diff-fonts'>
+            <div id='section-business-inline'>
+              <div id='section-font-1'>Infly</div>
+              <div id='section-font-2'>SHANGA</div>
+              <div id='section-font-3'>AGUIA NEGRA</div>
+            </div>
+            <div id='section-font-4'>ATHENAS</div>
+            <div id='section-font-5'>ADDERENCIA</div>
+          </div>
+        </section>
+
+        <section className='section-landing-tiny'>
+          <div className='section-landing-tiny-text'>
+            Tudo o que você precisa para levar a sua empresa para uma sociedade responsável.
+          </div>
+        </section>
+
+        <section className='section-step-by-step'>
+          <div className='section-step-by-step-text'>Passo a passo para nossa solução</div>
+          <div className='section-step-by-step-buttons'>
+            <ButtonHover />
+            <ButtonHover />
+            <ButtonHover />
+          </div>
+        </section>
+        <div className='section-step-by-step-text'>Receba pontuações de acordo com o seu trabalho e compartilhe com outras empresas</div>
+        <section className='section-landing-tiny'>
+          <div className='section-landing-tiny-text'>
+            Compartilhe o seu selo Plantüs de empresa responsável
+          </div>
+        </section>
+        <div className='image-boy-cellphone-box'>
+          <img id='image-boy-cellphone' src='https://res.cloudinary.com/dtqaoxaot/image/upload/v1636169693/Hack%20Faesa%202021/pessoa_thxtz5.svg' />
+        </div>
+        <div className='section-step-by-step-text'>Entre em contato e agende uma consultoria</div>
+        <section className='section-form'>
+          <div className='section-landing-tiny-text'>
+            Compartilhe o seu selo Plantüs de empresa responsável
+          </div>
+          <form id='form-consulting'>
+            <input className='form-input' type='string' placeholder='Nome' />
+            <input className='form-input' type='string' placeholder='Telefone' />
+            <input className='form-input' type='string' placeholder='Empresa' />
+            <input className='form-input' type='string' placeholder='Email' />
+            <div id='form-buttons'>
+              <button id='form-button'>Enviar</button>
+            </div>
+          </form>
+        </section>
+
+        <footer className='footer-landing'>
+          <div className='footer-landing-logo'>
+            <img id='footer-landing-logo-image' src='https://res.cloudinary.com/dtqaoxaot/image/upload/v1636160404/Hack%20Faesa%202021/Group_1_tyelge.svg' />
+          </div>
+          <div className='section-step-by-step-text'>Colaborando para um mundo mais unido</div>
+          <div className='section-landing-tiny-text'>
+            CNPJ: 123123213213-11
+          </div>
+        </footer>
+
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
